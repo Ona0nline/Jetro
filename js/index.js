5b7636005869571523db1311d0254ae7aa5df5ce
Ona0nline <onasihle123@gmail.com>
2024-09-07 20:35:04 +0200

    Basically done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
-new Typewriter('h1', {
-  strings: 'For Jetro',
-  autoStart: true,
+document.addEventListener('DOMContentLoaded', () => {
+  const typewriter = new Typewriter('#typewriter', {
+    loop: true,
+    delay: 20,
+  });
+
+  typewriter
+    .pauseFor(1500)
+    .typeString('For Jetro')
+    .pauseFor(200)
+    .deleteChars(10)
+    .start();
 });
